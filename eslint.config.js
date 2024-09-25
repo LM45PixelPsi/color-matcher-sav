0b0fd2675ef58413398a92ffdad4cd89f34ff169
LM45PixelPsi <leo.bpdr@gmail.com>
2024-09-25 12:31:30 +0200

    npm fix 3

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,8 @@
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import prettier from 'eslint-config-prettier';
-import globals from 'globals';
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
-/** @type {import('eslint').Linter.Config[]} */
+
 export default [
-	js.configs.recommended,
-	...svelte.configs['flat/recommended'],
-	prettier,
-	...svelte.configs['flat/prettier'],
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		ignores: ['build/', '.svelte-kit/', 'dist/']
-	}
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
